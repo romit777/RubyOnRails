7693d63eb3cdd55e0b1bba053c5b76b07ba17fa0
romit777 <romitranjanray@gmail.com>
2024-05-30 12:36:18 +0530

    Project commited

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
+  get 'gallery/index'
+  get 'gallery/checkout'
+  get 'gallery/search'
+
+  post 'gallery/checkout'
+
+  post 'gallery/search'
+  get 'gallery/purchase_complete'
+  resources :stores
+  get 'admin/login'
+  post 'admin/login'
+
+  get 'admin/logout'
+  devise_for :users
+  get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
